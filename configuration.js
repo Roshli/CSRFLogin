1374fa51a27f3c7562b7c1a7320c4f78e6db947c
Roshli <roshliperera@gmail.com>
2018-05-16 01:58:16 +0530

    CSRF

--- configuration.js
+++ configuration.js
@@ -1,16 +1,16 @@
 var csrf_token;
 
-//use this finction to retrive the response values coming form server side
+//to retrive the response values form server side the function is used
 function loadDOC(method,url,htmlTag)
 {
-    var xhttp = new XMLHttpRequest(); //create variable for store HTTP requests
-    xhttp.onreadystatechange = function() //excute when recive an answer from server side
+    var xhttp = new XMLHttpRequest(); //Store HTTP requests in a variable
+    xhttp.onreadystatechange = function() //When a request comes from server side it exicutes
     {
         if(this.readyState==4 && this.status==200)
         {
             console.log("CSRF token scuessfully fetched : "+this.responseText);
             document.getElementById(htmlTag).value = this.responseText;
-            //return this.responseText; //return response
+
             
             
         }
